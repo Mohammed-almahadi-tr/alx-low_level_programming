6ef2004cf59caa5385b658fb6189977c8bf5b181
Mohammed-almahadi-tr <mohammedalmahadi002@gmail.com>
2023-09-18 11:07:22 +0200

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - chage the value that the int hold.
+ *
+ * @n: pointer to int wanted.
+ *
+ * Return: nothing.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
