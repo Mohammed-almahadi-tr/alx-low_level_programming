0db6b70f775f9d8cd9e3689a67761f3229cb630c
Mohammed-almahadi-tr <mohammedalmahadi002@gmail.com>
2023-09-21 23:27:29 +0200

    8th

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - encode a string.
+ *
+ * @str: the string to be encoded.
+ *
+ * Return: encoded string.
+ */
+char *leet(char *str)
+{
+int i, j;
+
+char s1[] = "aAeEtTlL";
+char s2[] = "4433007711";
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (str[i] == s1[j])
+{
+str[i] = s2[j];
+}
+}
+}
+return (str);
+}
