0db8ee9a3ff62a7da4fd4e68147de4a28c579252
Mohammed-almahadi-tr <mohammedalmahadi002@gmail.com>
2023-10-07 19:45:16 +0200

    2ed

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,40 +1,23 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte. * @s: pointer to put the constant.
+ * _calloc - allocates memory for an array, initialized to 0
+ * @nmemb: number of elements
+ * @size: byte size of each element
  *
- * @b: constant.
- * @s: pointer to put the constant.
- * @n: max bytes to use.
- *
- * Return: s.
+ * Return: void pointer to array space
  */
-char *_memset(char *s, char b, unsigned int n)
-{
-char *ptr = s;
-
-while (n--)
-*s++ = b;
-return (ptr);
-}
-
-/**
- * *_calloc - allocates memory for an array, using malloc.
- *
- * @nmemb: array length
- * @size: size of each element
- *
- * Return: pointer
- */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *m;
-if (size == 0 || nmemb == 0)
-return (NULL);
-m = malloc(sizeof(int) * nmemb);
-if (m == 0)
-return (NULL);
-_memset(m, 0, sizeof(int) * nmemb);
-return (m);
+	char *p;
+
+	if (!nmemb || !size)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (!p)
+		return (NULL);
+	nmemb *= size;
+	while (nmemb--)
+		p[nmemb] = 0;
+	return (p);
 }
